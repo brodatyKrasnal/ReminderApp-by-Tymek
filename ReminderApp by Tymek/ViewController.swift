cb612974492dd41468cc1f48baa6ecdae5892105
Tymon Goleba Frygies <tymon.gf@hotmail.com>
2020-09-25 20:07:26 +0200

    Initial Commit

---
+++ ReminderApp by Tymek/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//	ReminderApp by Tymek : ViewController.swift by tymek on 25/09/2020 20:07.
+//	Copyright ©tymek 2020. All rights reserved.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
